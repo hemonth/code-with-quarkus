c3b2871087a948d68e629e62d56cdf5c06907a2d
jcnt219 <jb6528>
2020-12-25 11:37:53 -0600

    configurations for the app @ConfigProperty

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Quarkus.run(args);
     }
 }
--- src/main/java/com/hemonth/quarkus/ExampleResource.java
+++ src/main/java/com/hemonth/quarkus/ExampleResource.java
@@ -1,5 +1,6 @@
 package com.hemonth.quarkus;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,12 @@ import javax.ws.rs.core.MediaType;
 @RestController
 public class ExampleResource {
 
+    @ConfigProperty(name = "greeting.name")
+    private String greeting;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return greeting;
     }
 
     @GetMapping("/test")
